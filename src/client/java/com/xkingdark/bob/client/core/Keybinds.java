8955c11aa201930962953dd275c58d7baafba619
Nereziel <HelesicRic12@outlook.com>
2025-11-02 19:55:53 +0100

    1

--- src/client/java/com/xkingdark/bob/client/core/Keybinds.java
+++ src/client/java/com/xkingdark/bob/client/core/Keybinds.java
@@ -14,23 +14,21 @@ public class Keybinds {
     public static final KeyBinding TOAST_2;
     public static final KeyBinding TOAST_3;
 
-    static {
-        TOAST_3 = register("toast_3", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_3, "keybinds");
-        TOAST_2 = register("toast_2", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_2, "keybinds");
-        TOAST_1 = register("toast_1", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_1, "keybinds");
-        WAYSTONE = register("waystone", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_4, "keybinds");
-    };
+    private static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(Identifier.of(Main.MOD_ID, "keybinds"));
 
-    private static String categoryKey(String name) {
-        return Util.createTranslationKey("category", Identifier.of(Main.MOD_ID, name));
+    static {
+        TOAST_3 = register("toast_3", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_3);
+        TOAST_2 = register("toast_2", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_2);
+        TOAST_1 = register("toast_1", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_1);
+        WAYSTONE = register("waystone", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_4);
     };
 
     private static String keyName(String name) {
         return Util.createTranslationKey("key", Identifier.of(Main.MOD_ID, name));
     };
 
-    private static KeyBinding register(String name, InputUtil.Type inputType, int key, String category) {
-        KeyBinding keyBinding = new KeyBinding(keyName(name), inputType, key, categoryKey(category));
+    private static KeyBinding register(String name, InputUtil.Type inputType, int key) {
+        KeyBinding keyBinding = new KeyBinding(keyName(name), inputType, key, CATEGORY);
         return KeyBindingHelper.registerKeyBinding(keyBinding);
     };
 };
